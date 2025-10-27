13ad51584e13b74ae16baf11f6ae92d220273905
renzoPeraza <renzo.peraza@estudiantes.utec.edu.uy>
2025-10-27 07:44:39 -0300

    Usuarios

---
+++ Gatekeep/src/GateKeep.Api/Application/Usuarios/IUsuarioFactory.cs
@@ -0,0 +1,12 @@
+﻿using GateKeep.Api.Contracts.Usuarios;
+using GateKeep.Api.Domain.Entities;
+using GateKeep.Api.Domain.Enums;
+
+namespace GateKeep.Api.Application.Usuarios;
+
+public interface IUsuarioFactory
+{
+  Usuario CrearUsuario(UsuarioDto dto);
+  bool TryCrearUsuario(UsuarioDto dto, out Usuario? usuario);
+  bool EsRolValido(Rol rol);
+}
